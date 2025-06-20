0652fdc61255c879737de3c0a9e0b24fc4c8428d
Leonardo George <leonardogeorge30@gmail.com>
2025-06-20 13:18:52 -0300

    first commit

---
+++ PHP_lendo_byte/ler_nome.php
@@ -0,0 +1,27 @@
+<?php
+// Filesystem
+//f(fopnen, fclose, fread, fgets, file_exists,
+// is_file, is_dir, unlink, rename, copy, mkdir, rmdir) 
+// Ler um arquivo de texto e exibir seu conteúdo
+// Abrindo o arquivo nomes.txt para leitura 
+// Verifica se o arquivo existe
+/* 
+Segundo parametro é o modo de abertura do arquivo 
+    - "r" - Abre o arquivo para leitura. O ponteiro do arquivo é colocado no início do arquivo.
+    - "w" - Abre o arquivo para escrita. Se o arquivo já existir, ele será truncado (apagado) para zero bytes. Se o arquivo não existir, ele será criado.   
+    - "a" - Abre o arquivo para escrita. O ponteiro do arquivo é colocado no final do arquivo, ou seja, os dados serão adicionados ao final do arquivo. Se o arquivo não existir, ele será criado.
+    - "x" - Cria um novo arquivo para escrita. Se o arquivo já existir, a chamada falhará e retornará um erro.
+    - "r+" - Abre o arquivo para leitura e escrita. O ponteiro do arquivo é colocado no início do arquivo.
+*/
+// resource = recurso
+
+$arquivo = fopen("nomes.txt", "r");
+if ($arquivo) {
+    echo "Arquivo aberto com sucesso!<br>";
+} else {
+    echo "Erro ao abrir o arquivo!<br>";
+}
+print_r($arquivo);
+
+
+?>
