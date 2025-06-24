8124c6ac493038ece67bdc1c05275d2e462a6b5d
Leonardo George <leonardogeorge30@gmail.com>
2025-06-24 15:37:05 -0300

    while

--- PHP - lendo arqv em linha/ler_nome.php
+++ PHP - lendo arqv em linha/ler_nome.php
@@ -15,14 +15,9 @@ $bytes = filesize("nomes.txt");
 $nomes = fread($arquivo, $bytes); // Lê o arquivo inteiro
 */
 
-$nome1 = fgets($arquivo); // Lê a primeira linha do arquivo
-$nome2 = fgets($arquivo); // Lê a segunda linha do arquivo
-$nome3 = fgets($arquivo); // Lê a terceira linha do arquivo
-
-
-echo $nome1 . "<br>";
-echo $nome2 . "<br>";
-echo $nome3 . "<br>";
+while ($linha = fgets($arquivo)) {
+    echo $linha . "<br>"; // Lê linha por linha e exibe
+}
 
 fclose($arquivo); // Fecha o arquivo
 echo "<br>Arquivo fechado com sucesso!<br>";
